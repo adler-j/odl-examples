75603645e43e15064e72b4f3110e845fb918eece
Jonas Adler <jonasadl@kth.se>
2016-01-28 16:52:06 +0100

    ENH: improve doc of laplacian

--- pde_laplacian.py
+++ pde_laplacian.py
@@ -11,9 +11,9 @@ n = 100
 
 # Discrete reconstruction space
 domain = odl.uniform_discr([0, 0], [1, 1], [n, n])
-x, y = domain.grid.meshgrid(squeeze=True)
 
-# Geometry
+# Define right hand side
+x, y = domain.grid.meshgrid(squeeze=True)
 rhs_arr = np.zeros([n, n])
 rhs_arr[0, :] = 0.25 * np.sin(np.pi*y)
 rhs_arr[-1, :] = 1.00 * np.sin(np.pi*y)
@@ -22,9 +22,10 @@ rhs_arr[:, -1] = 0.50 * np.sin(np.pi*x)
 rhs_arr *= n**2
 rhs = domain.element(rhs_arr)
 
-# X-ray transform
-A = -odl.Laplacian(domain)
+# Define operator
+laplacian = -odl.Laplacian(domain)
 
+# Solve with conjugate gradient
 x = domain.zero()
-odl.solvers.conjugate_gradient(A, x, rhs, niter=300,
+odl.solvers.conjugate_gradient(laplacian, x, rhs, niter=300,
                                partial=odl.solvers.ShowPartial())
