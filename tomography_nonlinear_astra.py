ae5863c40ccac2b8c0b3d8f6082295965a37691e
Jonas Adler <jonasadl@kth.se>
2016-01-29 18:18:59 +0100

    ENH: improvements to the nonlinear examples

--- tomography_nonlinear_astra.py
+++ tomography_nonlinear_astra.py
@@ -16,7 +16,30 @@ import numpy as np
 
 # Internal
 import odl
-from tomography_helper import ForwardProjector
+
+
+def make_projector(n):
+    # Discrete reconstruction space
+    discr_reco_space = odl.uniform_discr([-20, -20, -20], [20, 20, 20],
+                                         [n]*3, dtype='float32')
+
+    # Geometry
+    src_rad = 1000
+    det_rad = 100
+    angle_intvl = odl.Interval(0, 2 * np.pi)
+    dparams = odl.Rectangle([-50, -50], [50, 50])
+    agrid = odl.uniform_sampling(angle_intvl, n)
+    dgrid = odl.uniform_sampling(dparams, [n]*2)
+    geom = odl.tomo.CircularConeFlatGeometry(angle_intvl, dparams,
+                                             src_rad, det_rad,
+                                             agrid, dgrid)
+
+    # X-ray transform
+    projector = odl.tomo.XrayTransform(discr_reco_space, geom,
+                                       backend='astra_cuda')
+    phantom = projector.domain.one()
+    projector._adjoint *= projector(phantom).inner(projector(phantom)) / phantom.inner(projector.adjoint(projector(phantom)))
+    return 0.08 * projector
 
 
 def mu(E):
@@ -24,51 +47,65 @@ def mu(E):
 
 
 class BeamHardeningProjector(odl.Operator):
-    def __init__(self, projector, energies):
+    def __init__(self, projector, energies, spectrum):
         self.projector = projector
         self.energies = energies
+
+        self.spectrum = spectrum
+        assert np.sum(spectrum) == 1.0
+
         super().__init__(projector.domain, projector.range, False)
 
-    def _call(self, x):
-        Ax = self.projector(x)
+    def _call(self, x, **kwargs):
+        Ahatx = kwargs.pop('Ahatx', None)
+        if Ahatx is None:
+            Ahatx = self.projector(x)
 
         result = self.range.zero()
-        for E in self.energies:
-            result += (-mu(E) * Ax).ufunc.exp()
-        return -(result / len(self.energies)).ufunc.log()
+        for E, I in zip(self.energies, self.spectrum):
+            result += I * (-mu(E) * Ahatx).ufunc.exp()
+        return -result.ufunc.log()
 
     def derivative(self, x):
-        Ax = self(x)
         Ahatx = self.projector(x)
+        Ax = self(x, Ahatx=Ahatx)
+
         scale = self.range.zero()
-        for E in self.energies:
-            scale += mu(E) * (Ax - mu(E) * Ahatx).ufunc.exp()
+        for E, I in zip(self.energies, self.spectrum):
+            scale += I * mu(E) * (Ax - mu(E) * Ahatx).ufunc.exp()
 
-        return (scale / len(self.energies)) * self.projector
+        return scale * self.projector
 
-n = 100
-
-# Create spaces
-d = odl.uniform_discr([0, 0], [1, 1], [n, n])
-ran = odl.uniform_discr([0, 0], [1, np.pi], [np.ceil(np.sqrt(2) * n), n])
-
-# Create phantom
-phantom = odl.util.shepp_logan(d, False)
+n = 400
 
 # Create projector
-proj = ForwardProjector(d, ran)
-A = BeamHardeningProjector(proj, np.linspace(0.3, 1, 3))
+proj = make_projector(n)
+
+# Create nonlinear projector
+energies = np.linspace(0.7, 1, 3)
+spectrum = np.array([0.5, 1.0, 0.5]) / 2.0
+A = BeamHardeningProjector(proj, energies, spectrum)
+
+# Create phantom
+phantom = odl.util.shepp_logan(A.domain, False)
 
 # Create data
 rhs = A(phantom)
 
-partial = (odl.solvers.util.ShowPartial(clim=[0.9, 1.1]) &
-           odl.solvers.util.ShowPartial(indices=np.s_[:, n//2]) &
+partial = (odl.solvers.util.ShowPartial(clim=[0.7, 1.3]) &
+           odl.solvers.util.ShowPartial(indices=np.s_[:, n//2, n//2]) &
+           odl.solvers.util.PrintTimingPartial() &
            odl.solvers.util.PrintIterationPartial())
 
 dA = A.derivative(phantom)
 norm2 = dA.adjoint(dA(phantom)).norm() / phantom.norm()
 
-x = d.one() * (phantom.ufunc.sum() / n**2)
-odl.solvers.landweber(A, x, rhs, 100, 0.05 / norm2, partial)
-#odl.solvers.landweber(dA, x, rhs, 100, 0.03 / norm2, partial)
+x = A.domain.one() * (phantom.ufunc.sum() / n**3)
+
+
+def make_positive(x):
+    x.ufunc.maximum(0.0, out=x)
+
+#raise Exception
+odl.solvers.landweber(A, x, rhs, niter=10, omega=1.0 / norm2, partial=partial,
+                      projection=make_positive)
